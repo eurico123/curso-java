3fbaa200450e24ddbad69c28a02488e0818c1ec9
Henrique Almeida Soares <hg515900@gmail.com>
2024-09-23 22:15:20 -0300

    aula 2 finalizada

---
+++ aula-2/CursoJava.java
@@ -0,0 +1,15 @@
+public class CursoJava {
+    public static void main(String[] args){
+
+        int n1=10;
+        int n2=20;
+        int n3=30;
+        int res=n1 + n2 + n3;
+        String nome="henrique";
+
+        System.out.print("Curso bambas \n");
+        System.out.println("Curso de Java");
+        System.out.printf("Nome:%s %n Idade:%d" , "henrique", 19 );
+        System.out.printf("%nValor da soma das variáveis: %d%n nome: %s", res, nome);
+    }
+}
---
+++ aula-3/CursoJava.java
@@ -0,0 +1,5 @@
+public class CursoJava {
+    public static void main(String[] args){
+        System.out.print("erfhged");
+    }
+}
