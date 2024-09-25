f926865832ec17d4bacde78a9dc19c09586dc589
Henrique Almeida Soares <hg515900@gmail.com>
2024-09-25 11:11:38 -0300

    aula 5 finalizada

---
+++ aula-5/CursoJavaCinco.java
@@ -0,0 +1,35 @@
+
+public class CursoJavaCinco {
+    public static void main(String[] args) {
+        
+        // FOR
+        /* 
+        for(int cont=5; cont > 0; cont--){
+            System.out.println(cont + " - CBF cursos");
+        };
+        */
+        
+        // WHILE
+        /* 
+        Scanner leitor = new Scanner(System.in);
+        int max=leitor.nextInt();
+
+        int cont=0;
+        while(cont < max){
+            System.out.println(cont + " - CBF cursos");
+            cont++;
+        };
+        */
+
+        // DO WHILE
+
+        int cont=0;
+        do{
+            System.out.println(cont + " - CBF cursos");
+            cont++;
+
+        }while(cont < 0);
+
+        System.out.println("fim do programa");
+    }
+}
