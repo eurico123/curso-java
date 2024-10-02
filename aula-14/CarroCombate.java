7b5bb62712dfe4bed9f2c9663e9fd370aab085ca
Henrique Almeida Soares <hg515900@gmail.com>
2024-10-02 00:03:06 -0300

    aula 14 finalizada

---
+++ aula-14/CarroCombate.java
@@ -0,0 +1,44 @@
+
+/*
+public class CarroCombate extends Carro{
+    private final int MAX_ARMAMENTO = 100;
+    private final int MIN_ARMAMENTO = 0;
+    private int qtdArmamento;
+
+    public CarroCombate(String nome, int blindagem){
+        super(nome);
+        setArmamento(true);
+        setBlindagem(blindagem);
+        this.qtdArmamento = 100;
+    };
+
+    public void setQtdArmamento(int qtdArmamento){
+        this.qtdArmamento += qtdArmamento;
+        if(this.qtdArmamento > MAX_ARMAMENTO){
+            this.qtdArmamento = MAX_ARMAMENTO;
+        };
+        if(this.qtdArmamento < MIN_ARMAMENTO){
+            this.qtdArmamento = MIN_ARMAMENTO;
+        };
+    };
+
+    public int getQtdArmamento(){
+        return this.qtdArmamento;
+    };
+
+    public void atirar(){
+        if(this.qtdArmamento > MIN_ARMAMENTO){
+            setQtdArmamento(-1);
+        }else{
+            System.out.println("Sem Munição!");
+        };
+    };
+
+    public void info(){
+        super.info();
+        System.out.printf("Qtd de arm...: %s%n", this.qtdArmamento);
+    };
+
+
+}
+*/
---
+++ aula-14/JavaQuatorze.java
@@ -0,0 +1,24 @@
+public class JavaQuatorze {
+    public static void main(String[] args) {
+        
+        /*
+        Carro c1 = new Carro("golf");
+        Carro c2 = new Carro("uno");
+        CarroCombate c3 = new CarroCombate("Kratos", 100);
+        CarroCombate c4 = new CarroCombate("Zeus", 60);
+
+        c3.atirar();
+        c3.atirar();
+        c3.atirar();
+        c4.sofrerDano(150);
+        c1.sofrerDano(5);
+
+
+        c1.info();
+        c2.info();
+        c3.info();
+        c4.info();
+        */
+
+    }
+}
