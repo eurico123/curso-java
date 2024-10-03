03c6aea51b5cc35c0d25bb007d89431d53a5099b
Henrique Almeida Soares <hg515900@gmail.com>
2024-10-02 23:16:36 -0300

    aula 15 finalizada

---
+++ aula-15/JavaQuinze.java
@@ -0,0 +1,11 @@
+public class JavaQuinze{
+    public static void main(String[] args) {
+        
+        /*
+        Aviao a1 = new Aviao("Voador", 1);
+
+        a1.info();
+        */
+
+    }
+}
