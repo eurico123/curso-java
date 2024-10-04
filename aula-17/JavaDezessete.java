13b5055ae42c9c1a7e28e9a0ab234719223c5f7b
Henrique Almeida Soares <hg515900@gmail.com>
2024-10-03 23:44:23 -0300

    aula 17 finalizada
    criação de uma calculadora de adição

---
+++ aula-17/JavaDezessete.java
@@ -0,0 +1,26 @@
+//import java.util.Scanner;
+
+public class JavaDezessete {
+    public static void main(String[] args) {
+        /*
+        Scanner scan = new Scanner(System.in);
+
+        Numero n1 = new Numero();
+        Numero n2 = new Numero();
+        Numero res = new Numero();  
+        String opc = "S";
+
+        while(opc.equals("s") || opc.equals("S")){
+            System.out.printf("%nDigite um valor 1: ");
+            n1.setValor(scan.nextInt());
+            System.out.printf("%nDigite um valor 2: ");
+            n2.setValor(scan.nextInt());
+            res.setValor(n1.getValor() + n2.getValor());
+            System.out.printf("%nA soma de %d com %d e: %d", n1.getValor(), n2.getValor(), res.getValor());
+            System.out.printf("%nDeseja continuar a somar valores? ");
+            opc = scan.next();
+            System.out.println("-----------------------------------------");
+        }
+        */
+    }
+}
