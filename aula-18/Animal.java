6dba84988fead16a280d639f49a9a0d3ceab1e24
Henrique Almeida Soares <hg515900@gmail.com>
2024-10-18 17:46:12 -0300

    aula 18 finalizada

---
+++ aula-18/Animal.java
@@ -0,0 +1,12 @@
+public class Animal implements SerVivo{
+    public Boolean vivo;
+    public Animal(){
+        this.vivo = true;
+    }
+
+    public void mover(){}
+    public void comer(int massa){}
+    public void info(){
+        System.out.printf("Vivo: %s", this.vivo ? "Sim" : "Não");
+    }
+}
---
+++ aula-18/JavaDezoito.java
@@ -0,0 +1,7 @@
+public class JavaDezoito {
+    public static void main(String[] args) {
+        Animal n1 = new Animal();
+
+        n1.info();
+    }
+}
